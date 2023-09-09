60dde56f57950a4a9f64f89e9e3c6ad3d003c0ae
Fortunate Omonuwa <omonuwafortunate@gmail.com>
2023-09-09 14:18:03 +0100

    final commit

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-//Get day of the week
-const dayOfTheWeek = document.getElementById("currentDayOfTheWeek");
-let getToDay = new Date();
-let presentDay = getToDay.getDay();
-if(presentDay == 1){
-    dayOfTheWeek.innerHTML = `Today is Day ${presentDay} of the week.`;
-} else if(presentDay == 2){
-    dayOfTheWeek.innerHTML = `Today is Day ${presentDay} of the week.`;
-} else if(presentDay == 3){
-    dayOfTheWeek.innerHTML = `Today is Day ${presentDay} of the week.`;
-} else if(presentDay == 4){
-    dayOfTheWeek.innerHTML = `Today is Day ${presentDay} of the week.`;
-} else if(presentDay == 5){
-    dayOfTheWeek.innerHTML = `Today is Day ${presentDay} of the week.`;
-} else if(presentDay == 6){
-    dayOfTheWeek.innerHTML = `Today is Day ${presentDay} of the week.`;
-} else {
-    dayOfTheWeek.innerHTML = `Today is Day ${presentDay} of the week.`;
+function updateDateTime() {
+  const dayOfTheWeek = document.querySelector(
+    '[data-testid="currentDayOfTheWeek"]'
+  );
+  const getCurrentTIme = document.querySelector(
+    '[data-testid="currentUTCTime"]'
+  );
+
+  const currentDate = new Date();
+
+  const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const currentDayOfWeek = daysOfWeek[currentDate.getUTCDay()];
+
+  const currentTime = new Date().toUTCString().split(" ")[4] + " UTC";
+
+  getCurrentTIme.textContent = currentTime;
+  dayOfTheWeek.textContent = currentDayOfWeek;
 }
+updateDateTime();
 
-//Get current Time in UTC
-const getCurrentTIme = document.getElementById("currentUTCTime");
-let getUTCTime = new Date();
-let currentUTCHour = getUTCTime.getUTCHours();
-let currentUTCMinutes = getUTCTime.getUTCMinutes();
-let currentUTCSeconds = getUTCTime.getUTCSeconds();
-let currentTime = `TIME: ${currentUTCHour}:${currentUTCMinutes}:${currentUTCSeconds} UTC`
-getCurrentTIme.innerHTML = currentTime;
-
+setInterval(updateDateTime, 1000);
